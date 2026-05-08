39f3103e614f0dafa3f2c9ab712671349cb69919
nformiransom100-ui <nformiransom100@gmail.com>
2026-05-08 16:01:41 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,1 @@
+const express=require('express');const path=require('path');const app=express();app.use(express.static(__dirname));app.get('*',(req,res)=>res.sendFile(path.join(__dirname,'index.html')));app.listen(process.env.PORT||3000,'0.0.0.0',()=>console.log('Running'));
